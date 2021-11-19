f64a0920e97c4a4b635c5a38309416d1f69f38ff
Justice Creativity <justiceisaac15@gmail.com>
2021-11-19 12:45:48 +0000

    Team Updated and paypal added

--- public/index.js
+++ public/index.js
@@ -1,38 +1,36 @@
-//inserting javascript date to the footer
+
+// const amountElement = document.getElementById("amount")
+// console.log(amountElement.value); 
+// paypal.
+//   Buttons({
+//     // Sets up the transaction when a payment button is clicked
+//     createOrder: function(data, actions) {
+//       return actions.order.create({
+//         purchase_units: [{
+//           amount: {
+//             value: amountElement.value   }
+//         }]
+//       });
+//     },
+//     // Finalize the transaction after payer approval
+//     onApprove: function(data, actions) {
+//       return actions.order.capture().then(function(details) {
+//         // Successful capture! For dev/demo purposes:
+//             console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+//             // var transaction = orderData.purchase_units[0].payments.captures[0];
+//             // alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+//         // When ready to go live, remove the alert and show a success message within this page. For example:
+//         // var element = document.getElementById('paypal-button-container');
+//         // element.innerHTML = '';
+//         // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+//         // Or go to another URL:  actions.redirect('thank_you.html');
+//       });
+//     }
+//   }).render('#paypal');
+
+
+
+  //inserting javascript date to the footer
 
 document.getElementById("date").appendChild(document.createTextNode(new Date().getFullYear()));
-
-// console.log('Client-side code running');
-
-// const button = document.getElementById('myDonate');
-// button.addEventListener('click', function() {
-//   console.log('button was clicked');
-// });
-
-
-// fetch('/clicked', {method: 'POST'})
-// .then(function(response) {
-//   if(response.ok) {
-//     console.log('Click was recorded');
-//     return;
-//   }
-//   throw new Error('Request failed.');
-// })
-// .catch(function(error) {
-//   console.log(error);
-// });
-
-
-// setInterval(function() {
-//   fetch('/clicks', {method: 'GET'})
-//     .then(function(response) {
-//       if(response.ok) return response.json();
-//       throw new Error('Request failed.');
-//     })
-//     .then(function(data) {
-//       document.getElementById('counter').innerHTML = `Button was clicked ${data.length} times`;
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//     });
-// }, 1000);
