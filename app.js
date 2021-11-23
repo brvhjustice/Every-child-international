82e3d057c86600af93d7a539f8d5e933ccc139aa
Justice Creativity <justiceisaac15@gmail.com>
2021-11-23 16:39:12 +0000

    update on Amount input

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 
 require('dotenv').config()
 const express = require('express');
-// const ejs = require('ejs');
-// const paypal = require('paypal-rest-sdk');
-// const MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/mydb";
-
-// paypal.configure({
-//     'mode': 'sandbox', //sandbox or live
-//      client_id: process.env.CLIENT_ID,
-//      client_secret: process.env.CLIENT_SECRET
-//      });
-  
 
 const app = express();
 app.set('view engine', 'ejs')
@@ -31,87 +20,6 @@ app.get("/views/donate.html", function(req, res) {
 })
 
 
-
-//  
-// app.get("/volunteer.html", function(req, res){
-//     res.sendFile(__dirname + "volunteer.html")
-// })
-
-// app.get("/success.html", function(req, res){
-//     res.sendFile(__dirname + "../index.html")
-//     console.log("Back Home Successfully");
-// })
-
-// app.post('/pay', (req, res) => {
-//     const create_payment_json = {
-//         "intent": "sale",
-//         "payer": {
-//             "payment_method": "paypal"
-//         },
-//         "redirect_urls": {
-//             "return_url": "http://localhost:3000/success",
-//             "cancel_url": "http://localhost:3000/cancel"
-//         },
-//         "transactions": [{
-//             "item_list": {
-//                 "items": [{
-//                     "name": "Every Child International",
-//                     "sku": "Donation",
-//                     "price": "25.00",
-//                     "currency": "USD",
-//                     "quantity": 1
-//                 }]
-//             },
-//             "amount": {
-//                 "currency": "USD",
-//                 "total": "25.00"
-//             },
-//             "description": "A donation for Every Child International"
-//         }]
-//     };
-
-//     paypal.payment.create(create_payment_json, function (error, payment) {
-//         if (error) {
-//             throw error;
-//         } else {
-//             for(let i = 0; i < payment.links.length;i++) {
-//                 if (payment.links[i].rel === 'approval_url') {
-//                     res.redirect(payment.links[i].href);
-                    
-//                 }
-//             }
-           
-//         }
-//     });
-// });
-
-// app.get('/success', (req, res) => {
-//     const payerId = req.query.PayerID;
-//     const paymentId = req.query.paymentId;
-
-//     const execute_payment_json = {
-//         "payer_id": payerId,
-//         "transactions": [{
-//             "amount": {
-//                 "currency": "USD",
-//                 "total": "25.00"
-//             }
-//         }]
-//     };
-//     paypal.payment.execute(paymentId, execute_payment_json, function(error, payment){
-//         if (error) {
-//             console.log(error.response);
-//             throw error;
-//         } else {
-//             // console.log("Get Payment Response");
-//             console.log(JSON.stringify(payment));
-//             res.sendFile(__dirname + "/public/success.html")
-//         }
-//     });
-// });
-
-
-// app.get('/cancel', (req, res) => res.sendFile(__dirname + "/public/cancel.html"));
 
 
 app.listen(process.env.PORT || 3000, function() {
--- public/index.js
+++ public/index.js
@@ -1,33 +1,34 @@
 
-// const amountElement = document.getElementById("amount")
-// console.log(amountElement.value); 
-// paypal.
-//   Buttons({
-//     // Sets up the transaction when a payment button is clicked
-//     createOrder: function(data, actions) {
-//       return actions.order.create({
-//         purchase_units: [{
-//           amount: {
-//             value: amountElement.value   }
-//         }]
-//       });
-//     },
-//     // Finalize the transaction after payer approval
-//     onApprove: function(data, actions) {
-//       return actions.order.capture().then(function(details) {
-//         // Successful capture! For dev/demo purposes:
-//             console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-//             // var transaction = orderData.purchase_units[0].payments.captures[0];
-//             // alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+const amountElement = document.getElementById("amount")
+console.log(amountElement.value); 
+paypal.
+  Buttons({
+    // Sets up the transaction when a payment button is clicked
+    createOrder: function(data, actions) {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: amountElement.value   }
+        }]
+      });
+    },
+    // Finalize the transaction after payer approval
+    onApprove: function(data, actions) {
+      return 
+      actions.order.capture().then(function(orderData) {
+        // Successful capture! For dev/demo purposes:
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+            var transaction = orderData.purchase_units[0].payments.captures[0];
+            alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
 
-//         // When ready to go live, remove the alert and show a success message within this page. For example:
-//         // var element = document.getElementById('paypal-button-container');
-//         // element.innerHTML = '';
-//         // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-//         // Or go to another URL:  actions.redirect('thank_you.html');
-//       });
-//     }
-//   }).render('#paypal');
+        // When ready to go live, remove the alert and show a success message within this page. For example:
+        // var element = document.getElementById('paypal-button-container');
+        // element.innerHTML = '';
+        // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+        // Or go to another URL:  actions.redirect('thank_you.html');
+      });
+    }
+  }).render('#paypal');
 
 
 
